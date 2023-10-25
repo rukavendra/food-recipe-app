55f13c6a3a1ca601f96bcbcba60bcbafe54d18e3
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-25 22:05:48 +0530

    BrowserRouter and Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import {Routes, Route, Outlet} from  'react-router-dom'
 import Home from './components/pages/Home'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import RecipeDetails from './components/pages/RecipeDetails'
 
 
+
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+
+}
 
 const App = () => {
-  return (<div className='bg-black'><Home/></div>)
+  return (<div className='bg-black'>
+    <Routes>
+      <Route path='/' element={<Layout/>}/>
+      <Route index element={<Home/>}/>
+      <Route path='recipes/:id' element={<RecipeDetails/> }/>
+    </Routes>
+  </div>)
 }
 
 export default App
